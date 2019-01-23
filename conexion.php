97c990725402966f3a50c2b6a5a63a52e31896e7
ing.Gustavo <aez-castanon@hotmail.com>
2019-01-22 23:09:30 -0600

    conexion
    conexion a servidor directo

--- conexion.php
+++ conexion.php
@@ -1,13 +1,11 @@
 <?php
-   $host="localhost";
-   $database="masterpoint";
-   $user="root";
-   $psw="";
-   $db=new mysqli($host,$user,$psw,$database);
- 
-   			if($db-> connect_error>0)
-   			   die("Error de conexion [".$db->connect_error."]");   
-   		
-   		//	else
-   		//		echo"Conexion Correcta";
-?>   			   
+  $host="160.153.43.65";
+  $database="modulo3";
+  $user="hector";
+  $psw="N3VERm0re2018@";
+  $db=new mysqli($host,$user,$psw,$database);
+  if($db->connect_error>0)
+     die("Error de conexion [".$db->connect_error."]");
+  //else
+     //echo "conexion correcta";
+?>
